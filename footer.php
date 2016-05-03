5cc1f584fd12c03d975f7de033f64780e2bdcaec
Pravin Tumsare <eew.pravin@gmail.com>
2016-05-03 21:06:52 +0530

    footer committed sagar changes

--- footer.php
+++ footer.php
@@ -12,36 +12,36 @@
     <div class="container">
         <div class="footer_wrapper row-fluid">
             <div class="contr_company span2">
-                <a href="http://#/about/">Company</a>
+                <a href="index.php">Company</a>
                 <ul>
-                    <li><a href="http://#/about/" title="About us">About us</a></li>
-                    <li><a href="http://#/process/" title="Process">Process</a></li>
+                    <li><a href="#" title="About us">About us</a></li>
+                    <li><a href="#" title="Process">Process</a></li>
                 </ul>
             </div>
             <div class="contr_services span3">
-                <a href="http://#/solutions/">Solutions</a>
+                <a href="Solutions.php">Solutions</a>
                 <ul>
-                    <li><a href="http://#/web-development/" title="Website Development">Website Development</a></li>
-                    <li><a href="http://#/mobile-development/" title="Mobile applications">Mobile applications</a></li>
-                    <li><a href="http://#/cms/" title="CMS solutions">CMS solutions</a></li>
-                    <li><a href="http://#/e-commerce/" title="E-commerce solutions">E-commerce solutions</a></li>
-                    <li><a href="http://#/cloud/" title="Cloud solutions">Cloud solutions</a></li>
-                    <li><a href="http://#/workflow-automation/" title="Business Process Automation">Business Process Automation</a></li>
-                    <li><a href="http://#/social-networking/" title="Social Networking">Social Networking</a></li>
-                    <li><a href="http://#/media-content-distribution/" title="Media Content Distribution">Media Content Distribution</a></li>
+                    <li><a href="WebDevelopmentServices.php" title="Website Development">Website Development</a></li>
+                    <li><a href="Mobiledevelopmentservices.php" title="Mobile applications">Mobile applications</a></li>
+                    <li><a href="CMSsolution.php" title="CMS solutions">CMS solutions</a></li>
+                    <li><a href="E-commerceSolutions.php" title="E-commerce solutions">E-commerce solutions</a></li>
+                    <li><a href="CloudSolution.php" title="Cloud solutions">Cloud solutions</a></li>
+                    <li><a href="CustomBusinessApplicationDevelopmentServices.php" title="Business Process Automation">Business Process Automation</a></li>
+                    <li><a href="SocialNetworkingservices.php" title="Social Networking">Social Networking</a></li>
+                    <li><a href="MediaContentDistribution.php" title="Media Content Distribution">Media Content Distribution</a></li>
                 </ul>
             </div>
             <div class="contr_expertise span3">
-                <a href="http://#/services/">Services</a>
+                <a href="Services.php">Services</a>
             </div>
             <ul class="footer_nav en">
-                <li><a href="http://#/">Home</a> </li>
-                <li><a href="http://#/solutions/">Solutions</a> </li>
-                <li><a href="http://#/services/">Services</a> </li>
-                <li><a href="http://#/expertise/">Expertise</a> </li>
-                <li><a href="http://#/portfolio/">Work</a> </li>
-                <li><a href="http://#/about/">Company</a> </li>
-                <li><a href="http://#/contact/">Contact</a> </li>
+                <li><a href="#">Home</a> </li>
+                <li><a href="Solutions.php">Solutions</a> </li>
+                <li><a href="Services.php">Services</a> </li>
+                <li><a href="WebExpertise.php">Expertise</a> </li>
+                <li><a href="#">Work</a> </li>
+                <li><a href="#">Company</a> </li>
+                <li><a href="#">Contact</a> </li>
             </ul>    
             <div class="footer_third_column span4">
                 <p><b style="text-transform: none; font-size: 20px;">CONTACT US</b></p>
@@ -50,54 +50,54 @@
             </div>
         </div>
     </div>
-    <div class="copyright">Copyright 2009 — 2016 © StableFlow. All rights reserved.</div>
+    <div class="copyright">Copyright 2009 — 2016 © EEW Technologies. All rights reserved.</div>
 </div>
 <div class="sb-slidebar sb-left">
     <nav>
         <ul class="sb-menu">
-            <li><a href="http://#/">Home</a>            </li>
-            <li><a href="http://#/services/">Services</a>            </li>
-            <li><a href="http://#/#" class="sb-toggle-submenu">Solutions<span class="sb-caret"></span></a>
+            <li><a href="index.php">Home</a>            </li>
+            <li><a href="Services.php">Services</a>            </li>
+            <li><a href="Solutions.php" class="sb-toggle-submenu">Solutions<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li><a href="http://#/solutions/">Solutions</a>                    </li>
-                    <li><a href="http://#/web-development/">Website Development</a>
+                    <li><a href="Solutions.php">Solutions</a>                    </li>
+                    <li><a href="WebDevelopmentServices.php">Website Development</a>
                     </li>
-                    <li><a href="http://#/mobile-development/">Mobile applications</a>
+                    <li><a href="Mobiledevelopmentservices.php">Mobile applications</a>
                     </li>
-                    <li><a href="http://#/cms/">CMS solutions</a>
+                    <li><a href="CMSsolution.php">CMS solutions</a>
                     </li>
-                    <li><a href="http://#/e-commerce/">E-commerce solutions</a>
+                    <li><a href="E-commerceSolutions.php">E-commerce solutions</a>
                     </li>
-                    <li><a href="http://#/cloud/">Cloud solutions</a>
+                    <li><a href="CloudSolution.php">Cloud solutions</a>
                     </li>
-                    <li><a href="http://#/workflow-automation/">Business Process Automation</a>
+                    <li><a href="CustomBusinessApplicationDevelopmentServices.php">Business Process Automation</a>
                     </li>
-                    <li><a href="http://#/social-networking/">Social Networking</a>
+                    <li><a href="SocialNetworkingservices.php">Social Networking</a>
                     </li>
-                    <li><a href="http://#/media-content-distribution/">Media Content Distribution</a>
+                    <li><a href="MediaContentDistribution.php">Media Content Distribution</a>
                     </li>
                 </ul>
             </li>
-            <li><a href="http://#/#" class="sb-toggle-submenu">Expertise<span class="sb-caret"></span></a>
+            <li><a href="#" class="sb-toggle-submenu">Expertise<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li><a href="http://#/expertise/">Expertise</a></li>
-                    <li><a href="http://#/expertise-web/">Web</a></li>
-                    <li><a href="http://#/expertise-mobile/">Mobile</a></li>
-                    <li><a href="http://#/expertise-cloud/">Cloud</a></li>
-                    <li><a href="http://#/expertise-api/">API</a></li>
+                    <li><a href=#">Expertise</a></li>
+                    <li><a href="#">Web</a></li>
+                    <li><a href="#">Mobile</a></li>
+                    <li><a href="#">Cloud</a></li>
+                    <li><a href="#">API</a></li>
                 </ul>
             </li>
-            <li><a href="http://#/#" class="sb-open-right">Work</a>            </li>
-            <li><a href="http://#/#" class="sb-toggle-submenu">Company<span class="sb-caret"></span></a>
+            <li><a href="#" class="sb-open-right">Work</a>            </li>
+            <li><a href="#" class="sb-toggle-submenu">Company<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li><a href="http://#/about/">About us</a></li>
-                    <li><a href="http://#/process/">Process</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="Process.php">Process</a></li>
                 </ul>
             </li>
-            <li><a href="http://#/contact/">Contact</a></li>
+            <li><a href="Contact.php">Contact</a></li>
         </ul>
     </nav>
-    <a class="sb-career-link" href="http://#/ru/">Career website</a>
+    <a class="sb-career-link" href="#">Career website</a>
     <br>
     <br>
     <br>
@@ -108,51 +108,50 @@
 <div class="sb-slidebar sb-right">
     <nav>
         <ul class="sb-menu">
-            <li class=""><a href="http://#/portfolio/">All Items</a></li>
+            <li class=""><a href="#">All Items</a></li>
 
-            <li class=""><a href="http://#/#" class="sb-toggle-submenu">By Business Domain<span class="sb-caret"></span></a>
+            <li class=""><a href="#" class="sb-toggle-submenu">By Business Domain<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li class=""><a href="http://#/portfolio/ecommerce-retail/">E-commerce &amp; Retail</a></li>
-                    <li class=""><a href="http://#/portfolio/travel-booking/">Travel &amp; Booking</a></li>
-                    <li class=""><a href="http://#/portfolio/communities-networks/">Communities &amp; Networks</a></li>
-                    <li class=""><a href="http://#/portfolio/telecommunications/">Telecommunications</a></li>
-                    <li class=""><a href="http://#/portfolio/arts-entertainment/">Arts &amp; Entertainment</a></li>
-                    <li class=""><a href="http://#/portfolio/media/">Media Distribution</a></li>
-                    <li class=""><a href="http://#/portfolio/news-publications/">News and Publications</a></li>
-                    <li class=""><a href="http://#/portfolio/customer-management/">Customer Management</a></li>
-                    <li class=""><a href="http://#/portfolio/elearning/">eLearning &amp; Online Training</a></li>
-                    <li class=""><a href="http://#/portfolio/sport-health/">Sport &amp; Health</a></li>
-                    <li class=""><a href="http://#/portfolio/business/">Business</a></li>
+                    <li class=""><a href="#">Travel &amp; Booking</a></li>
+                    <li class=""><a href="Community&Network.php">Communities &amp; Networks</a></li>
+                    <li class=""><a href="#">Telecommunications</a></li>
+                    <li class=""><a href="#">Arts &amp; Entertainment</a></li>
+                    <li class=""><a href="#">Media Distribution</a></li>
+                    <li class=""><a href="#">News and Publications</a></li>
+                    <li class=""><a href="#">Customer Management</a></li>
+                    <li class=""><a href="#">eLearning &amp; Online Training</a></li>
+                    <li class=""><a href="#">Sport &amp; Health</a></li>
+                    <li class=""><a href="#">Business</a></li>
                 </ul>
             </li>
-            <li class=""><a href="http://#/#" class="sb-toggle-submenu">By Type<span class="sb-caret"></span></a>
+            <li class=""><a href="#" class="sb-toggle-submenu">By Type<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li class=""><a href="http://#/portfolio/website/">Website</a></li>
-                    <li class=""><a href="http://#/portfolio/responsive-web-design/">Responsive Web Design</a></li>
-                    <li class=""><a href="http://#/portfolio/ecommerce/">E-commerce</a></li>
-                    <li class=""><a href="http://#/portfolio/cms/">CMS</a></li>
-                    <li class=""><a href="http://#/portfolio/saas/">SaaS</a></li>
-                    <li class=""><a href="http://#/portfolio/mobile-application/">Mobile Application</a></li>
-                    <li class=""><a href="http://#/portfolio/mobile-game/">Mobile Game</a></li>
-                    <li class=""><a href="http://#/portfolio/smart-tv/">Smart TV Application</a></li>
+                    <li class=""><a href="#">Website</a></li>
+                    <li class=""><a href="#">Responsive Web Design</a></li>
+                    <li class=""><a href="#">E-commerce</a></li>
+                    <li class=""><a href="#">CMS</a></li>
+                    <li class=""><a href="#">SaaS</a></li>
+                    <li class=""><a href="#">Mobile Application</a></li>
+                    <li class=""><a href="#">Mobile Game</a></li>
+                    <li class=""><a href="#">Smart TV Application</a></li>
                 </ul>
             </li>
 
-            <li class=""><a href="http://#/#" class="sb-toggle-submenu">By Technology Focus<span class="sb-caret"></span></a>
+            <li class=""><a href="#" class="sb-toggle-submenu">By Technology Focus<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li class=""><a href="http://#/portfolio/php/">PHP</a></li>
-                    <li class=""><a href="http://#/portfolio/html5/">HTML5</a></li>
-                    <li class=""><a href="http://#/portfolio/android/">Android</a></li>
-                    <li class=""><a href="http://#/portfolio/ios/">iOS</a></li>
-                    <li class=""><a href="http://#/portfolio/samsung-smart-tv/">Samsung Smart TV</a></li>
-                    <li class=""><a href="http://#/portfolio/google-tv/">Google TV</a></li>
+                    <li class=""><a href="#">PHP</a></li>
+                    <li class=""><a href="#">HTML5</a></li>
+                    <li class=""><a href="#">Android</a></li>
+                    <li class=""><a href="#">iOS</a></li>
+                    <li class=""><a href="#">Samsung Smart TV</a></li>
+                    <li class=""><a href="#">Google TV</a></li>
                 </ul>
             </li>
-            <li class=""><a href="http://#/#" class="sb-toggle-submenu">By Framework<span class="sb-caret"></span></a>
+            <li class=""><a href="#" class="sb-toggle-submenu">By Framework<span class="sb-caret"></span></a>
                 <ul class="sb-submenu">
-                    <li class=""><a href="http://#/portfolio/yii/">Yii</a></li>
-                    <li class=""><a href="http://#/portfolio/wordpress/">WordPress</a></li>
-                    <li class=""><a href="http://#/portfolio/bootstrap/">Bootstrap</a></li>
+                    <li class=""><a href="#">Yii</a></li>
+                    <li class=""><a href="#">WordPress</a></li>
+                    <li class=""><a href="#">Bootstrap</a></li>
                 </ul>
             </li>
         </ul>
